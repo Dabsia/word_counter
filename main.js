cf21a562847912e9ba51ae0c61a98114e5b09d46
Dabsia <dabojohnson98@gmail.com>
2021-09-01 09:18:31 +0100

    Final Updates

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-const textArea = document.getElementById('word');
-const words_count = document.getElementById('show')
-
-
-textArea.oninput = () => {
-    let words = textArea.value.split(' ').filter((item) =>{
-        return item != '';
-    });
-    words_count.textContent = words.length;
+		function countWord() {
+			var words = document.getElementById("word").value;
+			// Initialize the word counter
+			var count = 0;
+			// Split the words on each space character
+			var split = words.split(' ');
+            console.log(split)
+            
+			// Loop through the words and increase the counter when each split word is not empty
+			for (var i = 0; i < split.length; i++) {
+				if (split[i] != "") {
+					count += 1;
+				}
+			}
+			// Display it as output
+			var show = document.getElementById("show");
+            show.textContent = count;
 }
